bc85e58209b356a01ad063de81229785c9d85786
zuspect <artemmishenko@mail.ru>
2024-07-09 12:58:39 +0400

    add timezone

--- config/dayjs.js
+++ config/dayjs.js
@@ -1,9 +1,12 @@
 import 'dayjs/locale/ru.js'
 import dayjs from 'dayjs'
 import isToday from 'dayjs/plugin/isToday.js'
+import timezone from 'dayjs/plugin/timezone.js'
 
 dayjs.locale('ru')
 dayjs.extend(isToday)
+dayjs.extend(timezone)
+dayjs.tz.setDefault('Europe/Saratov')
 
 console.log('🕗', dayjs().format('DD MMMM YYYY, HH:mm'))
 
